0867e4ccbee1e20e6821fe5032a59f0d182b6a5e
YashpalGawali <gawaliyashpal@gmail.com>
2023-10-05 17:10:56 +0530

    05 Oct Fir commit

--- src/app/MyComponents/Company/viewcompany/viewcompany.component.ts
+++ src/app/MyComponents/Company/viewcompany/viewcompany.component.ts
@@ -1,28 +1,28 @@
-import { Component,OnInit } from '@angular/core';
+import { AfterViewInit, Component,OnInit } from '@angular/core';
 
 import { Route, Router } from '@angular/router';
 import { CompanyService } from 'src/app/Services/company.service';
 import DataTable from 'datatables.net';
-
+declare const $: any;
 
 @Component({
   selector: 'app-viewcompany',
   templateUrl: './viewcompany.component.html',
   styleUrls: ['./viewcompany.component.css']
 })
-export class ViewcompanyComponent {
+export class ViewcompanyComponent implements OnInit, AfterViewInit {
 
   complist : any;
   comp_id : any;
 
-   
   constructor(private compserv : CompanyService, private router : Router ) { }
-
+  ngAfterViewInit(): void {
+    $('#comptable').DataTable();
+  }
+   
   ngOnInit(): void {
-   $(document).ready(function(){
-      $('#comptable').DataTable({});
-    })
     this.compserv.getAllCompanies().subscribe(data=>this.complist=data);
+    
   }
 
   getCompById(cid : any)
